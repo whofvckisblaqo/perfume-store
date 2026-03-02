feee8805d6775304981116ec5bb0434c0dcdc7d5
kingsleyonyekwelu <kimonyekwelu@gmail.com>
2026-03-02 15:32:52 +0100

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,18 +2,20 @@ import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import Collection from "@/components/Collection";
 import About from "@/components/About";
-import Ingredients from "@/components/Ingridents";
+import Ingredients from "@/components/Ingredients";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import CartDrawer from "@/components/CartDrawer";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <CartDrawer />
       <Hero />
       <Collection />
-      <About />  
-      <Ingredients /> 
+      <About />
+      <Ingredients />
       <Contact />
       <Footer />
     </main>
